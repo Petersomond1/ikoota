174d0c4424440ed723242ad3c345c4d85a6ce998
Petersomond1 <petersomond@gmail.com>
2024-12-29 01:25:22 -0500

    Admin layout and css, done

--- ikootaclient/src/components/admin/Admin.jsx
+++ ikootaclient/src/components/admin/Admin.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './admin.css'
+import React, { useState } from 'react';
+import './admin.css';
 
 const Admin = () => {
+  const [selectedItem, setSelectedItem] = useState('Towncrier');
+
   return (
-    <div>Admin</div>
-  )
+    <div className='admin_container'>
+      <div className="navbar">navbar</div>
+      <div className="admin_viewport">
+        <div className="admin_sidebar">
+          <div className="admin_sidebar_item" onClick={() => setSelectedItem('Towncrier')}><p>Towncrier</p></div>
+          <div className="admin_sidebar_item" onClick={() => setSelectedItem('Iko')}><p>Iko</p></div>
+          <div className="admin_sidebar_item" onClick={() => setSelectedItem('Auth')}><p>Auth</p></div>
+        </div>
+        
+        <div className="admin_controls_body">
+          {selectedItem === 'Towncrier' && <div className="admin_controls_header">Towncrier Management</div>}
+          {selectedItem === 'Iko' && <div className="admin_controls_header">Iko Controls</div>}
+          {selectedItem === 'Auth' && <div className="admin_controls_header">Authorization Controls</div>}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Admin
+export default Admin;
