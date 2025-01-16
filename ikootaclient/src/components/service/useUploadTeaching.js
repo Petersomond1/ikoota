4d7f31eb0cc4b8301f97dca54bef5dd91466a874
Petersomond1 <petersomond@gmail.com>
2025-01-16 07:55:23 -0500

    minor changes

--- ikootaclient/src/components/service/useUploadTeaching.js
+++ ikootaclient/src/components/service/useUploadTeaching.js
@@ -1,13 +1,13 @@
 //useUploadTeaching.js
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import axios from "axios";
+import api from "./api";
 
 export const useUploadTeaching = () => {
   const queryClient = useQueryClient();
 
   return useMutation(
     async (data) => {
-      const response = await axios.post("/api/teachings", data, {
+      const response = await api.post("/teachings", data, {
         headers: { "Content-Type": "multipart/form-data" },
       });
       return response.data;
